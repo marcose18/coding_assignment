3851b53ace33de70c6528dd27e53ab48c1c6f03a
Abhishek Pratap <abhishekpratap.singh@edfora.com>
2018-05-19 00:39:39 +0530

    Second Revision

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 
@@ -7,29 +8,22 @@ def print_ratings_of_movie(url, movie_name):
     soup = BeautifulSoup(response.text, "html.parser")
     movie_list = soup.find_all('div', class_ = 'ratingValue')
     if(len(movie_list) == 0):
-        print("Ratings for the movie is not available at IMDB, Needs atleast 5 ratings")
+        print("Ratings for the movie", movie_name, "is not available at IMDB, Needs atleast 5 ratings")
     else :
         print("Ratings of the movie", movie_name, "is:", movie_list[0].strong.span.text)
+        #comment below line if printing total votes is not desired 
         print(movie_list[0].strong['title'])
 
 if __name__ == '__main__' :
     base_url = 'https://www.imdb.com'
-    url = 'https://www.imdb.com/find?ref_=nv_sr_fn&q=shawshank+redemption&s=all'
+    input = '+'.join(sys.argv[1].split(' '))
+    url = 'https://www.imdb.com/find?ref_=nv_sr_fn&q=' + input + '&s=all'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     movie_list = soup.find_all('tr', class_ = 'findResult odd')
     movie_list += soup.find_all('tr', class_ = 'findResult even')
-    # print(len(movie_list), type(movie_list))
-    # print(movie_list)
     for movie in movie_list:
         if(movie.a['href'].startswith('/title')):
             soup = BeautifulSoup(str(movie),"html.parser")
             movie_name = soup.find_all('td')[1]
-            # print(movie_name.text) 
-            # print(movie.a['href'])
-            # print(base_url + movie.a['href'])
             print_ratings_of_movie(base_url + movie.a['href'], movie_name.text)
-    # print(len(movie_list), type(response.text), type(str(movie_list[0])), movie_list[0].a['href'])
-    # soup = BeautifulSoup(str(movie_list[0]), "html.parser")
-    # link = soup.find('a', href = True)
-    # print(link['href'], type(link['href']))
